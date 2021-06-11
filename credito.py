6c21bd585c01e60f2eb0866f0a3e4bfc5f087176
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2021-06-11 07:46:28 -0300

    Retirado partes inutilizadas do código

--- credito.py
+++ credito.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""
-Created on Sun May  2 20:30:52 2021
-
-@author: T-Gamer
-"""
-
-
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
@@ -77,14 +68,14 @@ dados_teste['grau_instrucao_companheiro'] = dados_teste['grau_instrucao_companhe
 dados_teste['ocupacao'] = dados_teste['ocupacao'].fillna(2.0)  
 dados_teste['meses_na_residencia'] = dados_teste['meses_na_residencia'].fillna(10.0)  
 
-'''
+
 # Analise de dados
 
 for coluna in dados.columns:
     print('Valores de ' + coluna)
     print(dados[coluna].value_counts())
     
-'''   
+  
 
 # One Hot Encoding
 
@@ -199,16 +190,16 @@ X_train, X_test, y_train,y_test = train_test_split(X,y, train_size=15000, random
 
 # Ajustar escala
 
-    scaler = MinMaxScaler()
-    scaler.fit(X_train)
+scaler = MinMaxScaler()
+scaler.fit(X_train)
     
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X = scaler.transform(X)
-    dados_teste = scaler.transform(dados_teste)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+scaler = MinMaxScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+dados_teste = scaler.transform(dados_teste)
 
 # Função para ver resultados
 
@@ -220,31 +211,13 @@ def resultados(grid):
     return results
 
    
-
-
-'''
-# Validação cruzada
-
-for k in range(550,600):
-    
-    classificador = KNeighborsClassifier(
-        n_neighbors = k)
-    
-    classificador = classificador.fit(X_train,y_train)
-    scores = cross_val_score(classificador,X,y, cv=5)
-    print('k =%2d' % k,
-          'scores =', scores,
-          'acuracia média = %6.1f' % (100*sum(scores)/5)
-          )
-
-
 # Grid KNN
 
 # Melhores Parametros {{'algorithm': 'auto', 'n_neighbors': 161, 'p': 1, 'weights': 'distance'}
 parameters = {
-    "p": [1],
+    "p": [1,2],
     "n_neighbors": list(range(121,222,20)),
-    'weights': ['distance'],
+    'weights': ['distance','uniform'],
     'algorithm':['auto']
     }
 
@@ -258,38 +231,14 @@ bestknn = gridknn.best_estimator_
 bestknn.fit(X,y)
 respostaknn = bestknn.predict(dados_teste)
 
-
-# Grid SVM
-
-'''
-'''
-parameters = {
-    "kernel": ['linear', 'poly', 'rbf','sigmoid'],
-    "C": [100,110,120],
-    "gamma": [0.01,0.001,0.0001]
-    }
-
-grid = GridSearchCV(SVC(), parameters, cv=4, verbose=2, n_jobs=-1)
-grid.fit(X, y)
-
-'''
-# Melhores Parametros {'C': 110, 'gamma': 0.001, 'kernel': 'sigmoid'}
-
-'''
-# SVM Kernel
-
-classificadorSVM = SVC(C=110, gamma=0.001, kernel='poly',degree=1)
-classificadorSVM.fit(X,y)
-resposta_svm = classificadorSVM.predict(dados_teste)
-'''
 # Grid Random Forest
 
 # Melhores Parametros {"max_depth":[8], "max_features": ['auto'], "n_estimators": [500], "min_samples_leaf":[10]} (0.5906)
 parameters = {
-    "max_depth":[8],
-    "max_features": ['auto'],
+    "max_depth":[2,8,14],
+    "max_features": ['auto','sqrt','log2'],
     "n_estimators": [500],
-    "min_samples_leaf":[10]
+    "min_samples_leaf":[2,10,18]
     }
 
 gridRF = GridSearchCV(RandomForestClassifier(), parameters, cv=10, verbose=2, n_jobs = -1)
@@ -301,57 +250,6 @@ bestRF.fit(X,y)
 respostaRF = bestRF.predict(dados_teste)
     
 
-'''
-'''
-# Grid Gradient Boost Classifier
-'''
-# Melhores Parametros {'learning_rate': 0.01, 'n_estimators': 500,'max_depth': 4}
-parameters = {
-    #'learning_rate': [0.15,0.1,0.05,0.01,0.005,0.001],
-    #"n_estimators": [100,250,500,750,1000,1250,1500,1750],
-    #'max_depth': [2,3,4,5,6,7]
-    }
-
-grid = GridSearchCV(GradientBoostingClassifier(learning_rate = 0.01, n_estimators = 500, max_depth=4, min_samples_split=2, min_samples_leaf=1), parameters, cv=4, verbose=2, n_jobs = -1)
-grid.fit(X, y)
-'''
-'''
-# Gradient Boost Classifier
-
-classificadorGB = GradientBoostingClassifier(loss='exponential',
-    n_estimators=245,
-    learning_rate=1.0,
-     max_depth=1,
-     random_state=0,
- max_leaf_nodes=10)
-
-classificadorGB.fit(X,y)
-respostaGB = classificadorGB.predict(dados_teste)
-
-
-# Grid Logistic Regression
-
-# Melhores Parametros {'C': 0.1, 'solver': 'lbfgs'}
-parameters = {
-    'solver': ['newton-cg', 'lbfgs', 'liblinear'],
-    #"penalty": [100,250,500,750,1000,1250,1500,1750],
-    'C': [100, 10, 1.0, 0.1, 0.01]
-    }
-
-grid = GridSearchCV(LogisticRegression(max_iter = 1000000), parameters, cv=100, verbose=2, n_jobs = -1)
-grid.fit(X, y)
-
-'''
-'''
-# Logistic Regression
-
-classificadorLR = LogisticRegression(C= 1.0, solver='liblinear', max_iter = 10000000)
-classificadorLR.fit(X,y)
-respostaLR = classificadorLR.predict(dados_teste)
-'''
-
-
-# Confusion Matrix
 
 # Resposta para csv
 resposta_final = respostaRF
